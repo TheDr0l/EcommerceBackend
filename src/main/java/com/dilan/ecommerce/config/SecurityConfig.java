910467f00217de81148ad2b1b161a2b88cb52b09
TheDr0l <dilanosorio0717@gmail.com>
2025-11-24 17:44:34 -0500

    se permiten todas las peticiones de options

--- src/main/java/com/dilan/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/dilan/ecommerce/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.dilan.ecommerce.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -40,6 +41,7 @@ public class SecurityConfig {
                 }))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/productos/**").permitAll() // <-- temporalmente público
                         .anyRequest().authenticated())
